cd5475e3844ae29407fb096eda0f8905138f9d69
Iman Mesgaran <AIC_SIS@yahoo.com>
2020-02-04 14:10:40 +0330

    custom fonts page edited.

--- flutter_learning_01/lib/pages/custom.fonts.dart
+++ flutter_learning_01/lib/pages/custom.fonts.dart
@@ -30,14 +30,148 @@ class _MyCustomFontsPage extends State<CustomFontsPage> {
       // body: Center(
       //   child: Text("You pressed the button $_counter times."),
       // ),
-      body: new Center(
-          child: Text(
-        'this is a custom font.',
-        style: new TextStyle(
-            fontFamily: 'OpenSans',
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold),
-      )),
+      body: getFontsOnWeight(),
+    );
+  }
+
+  ListView getFontsOnWeight() {
+    return ListView(
+      children: <Widget>[
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: new Center(
+                child: Text('Default font',
+                    style: new TextStyle(
+                      fontFamily: 'OpenSans',
+                    ))),
+          ),
+          color: Colors.pink[50],
+        ),
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: new Center(
+                child: Text(
+              'Light font',
+              style: new TextStyle(
+                  fontFamily: 'OpenSans', fontWeight: FontWeight.w300),
+            )),
+          ),
+          color: Colors.pink[100],
+        ),
+        Container(
+          child: new Center(
+              child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              'Italic font',
+              style: new TextStyle(
+                  fontFamily: 'OpenSans', fontStyle: FontStyle.italic),
+            ),
+          )),
+          color: Colors.pink[200],
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Light Italic font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          color: Colors.pink[300],
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Semi Bold font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans', fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          color: Colors.pink[400],
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Semi Bold Italic font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.w600,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+          ),
+          color: Colors.pink,
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Bold font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans', fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+          color: Colors.pink[600],
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Bold Italic font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+          color: Colors.pink[700],
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Extra Bold font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans', fontWeight: FontWeight.w800),
+              ),
+            ),
+          ),
+          color: Colors.pink[800],
+        ),
+        Container(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Extra Bold Italic font',
+                style: new TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w800),
+              ),
+            ),
+          ),
+          color: Colors.pink[900],
+        ),
+      ],
     );
   }
 }
